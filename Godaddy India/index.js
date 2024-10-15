6b82d032db5cf2b2f5d1c59cc1f21495ae23bb75
Shantanu <75296055+shantanusoni72@users.noreply.github.com>
2024-10-15 19:53:58 +0530

    Add files via upload

--- Godaddy India/index.js
+++ Godaddy India/index.js
@@ -1,8 +1,44 @@
-function openCity(cityName) {
-    var i;
-    var x = document.getElementsByClassName("city");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";  
-    }
-    document.getElementById(cityName).style.display = "block";  
+function opentab(tabName) {
+  var i;
+  var x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
+  document.getElementById(tabName).style.display = "flex";
+}
+
+function handleDropdown(dropdownName, command) {
+  var i;
+  var x = document.getElementsByClassName("dropdown");
+  if (command == "close") {
+    for (i = 0; i < x.length; i++) {
+      if (x[i].style.display == "flex") {
+        x[i].style.display = "none";
+      }
+    }
+  } else if (command == "open") {
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(dropdownName).style.display = "flex";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdownToggle = document.querySelector(".dropdown-toggle");
+  const dropdownMenu = document.querySelector(".dropdown-menu");
+
+  // Toggle dropdown visibility
+  dropdownToggle.addEventListener("click", function () {
+      dropdownMenu.classList.toggle("show");
+  });
+
+  // Close the dropdown when clicking outside
+  window.addEventListener("click", function (event) {
+      if (!event.target.matches('.dropdown-toggle')) {
+          if (dropdownMenu.classList.contains("show")) {
+              dropdownMenu.classList.remove("show");
+          }
+      }
+  });
+});
