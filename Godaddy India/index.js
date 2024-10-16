38b97d55ea8d3401f5c915dcdf8613bd3dc678d2
Shantanu <75296055+shantanusoni72@users.noreply.github.com>
2024-10-16 20:01:54 +0530

    Add files via upload

--- Godaddy India/index.js
+++ Godaddy India/index.js
@@ -1,12 +1,19 @@
+// Change Tabs
 function opentab(tabName) {
   var i;
   var x = document.getElementsByClassName("tab");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  document.getElementById(tabName).style.display = "flex";
+  const isMobile = window.innerWidth <= 450;
+  if (isMobile) {
+    document.getElementById(tabName).style.display = "block";
+  } else {
+    document.getElementById(tabName).style.display = "flex";
+  }
 }
 
+// Handle Dropdown
 function handleDropdown(dropdownName, command) {
   var i;
   var x = document.getElementsByClassName("dropdown");
@@ -24,7 +31,54 @@ function handleDropdown(dropdownName, command) {
   }
 }
 
+// Hide Dropdown onClick Logo
 var logo = document.querySelector(".logo");
-logo.addEventListener("click", function() {
+logo.addEventListener("click", function () {
   handleDropdown("null", "close");
 })
+
+// Hide SearchSVG onClick SearchInput
+var searchInput = document.getElementById("searchInput");
+var searchSVG = document.getElementById("searchSVG");
+
+searchInput.addEventListener('click', function () {
+  searchSVG.style.display = "none";
+  searchInput.style.transition = "0.2s";
+  searchInput.style.paddingInline = "2%";
+})
+
+// Show SearchSVG onClick Document
+document.addEventListener('click', function (event) {
+  if (event.target !== searchInput && event.target !== searchSVG) {
+    searchSVG.style.display = "block";
+    searchInput.style.transition = "0.2s";
+    searchInput.style.paddingInline = "5%";
+  }
+});
+
+var heroBtnOne = document.getElementById("HeroBtnOne");
+var heroBtnTwo = document.getElementById("HeroBtnTwo");
+
+var heroSVGone = document.getElementById("HeroSVGone");
+var heroSVGtwo = document.getElementById("HeroSVGtwo");
+
+heroBtnOne.addEventListener("mouseover", function () {
+  heroSVGone.style.display = "block";
+  heroSVGone.style.transition = "0.2s";
+})
+
+heroBtnTwo.addEventListener("mouseover", function () {
+  heroSVGtwo.style.display = "block";
+  heroSVGtwo.style.transition = "0.2s";
+})
+
+// Hide RightArrow onHover Document
+
+// document.addEventListener('mouseover', function (event) {
+//   if (event.target !== heroBtnOne || event.target !== heroBtnTwo) {
+//     if(heroSVGone.style.display === "block" || heroSVGtwo.style.display === "block") {
+//       heroSVGone.style.display = "none";
+//       heroSVGtwo.style.display = "none";
+//     }
+//   }
+// });
