508d6e42a2fc2973f35dc0191dd3259488c37285
Shantanu <75296055+shantanusoni72@users.noreply.github.com>
2024-10-18 20:04:25 +0530

    Add files via upload

--- Godaddy India/index.js
+++ Godaddy India/index.js
@@ -14,24 +14,39 @@ function opentab(tabName) {
 }
 
 // Handle Dropdown
-function handleDropdown(dropdownId, action) {
+function handleDropdown(dropdownId, dropdownToggle, action) {
+  // Dropdowns
   const dropdown = document.getElementById(dropdownId);
-  var header = document.querySelector(".header");
+  const allDropdowns = document.querySelectorAll('.dropdown');
 
+  var DropdownToggle = document.getElementById(dropdownToggle);
+  var allDropdownToggles = document.querySelectorAll('.toggle');
+  var PricingLink = document.getElementById("pricingLink");
+
+  // Close all dropdowns
+  allDropdowns.forEach(d => {
+    if (d.id !== dropdownId) {
+      d.classList.remove('open');
+    }
+  });
+
+  // Close all toggles
+  allDropdownToggles.forEach(d => {
+    if (d.id !== DropdownToggle) {
+      d.classList.remove('dropdownToggle');
+    }
+  });
+
+  PricingLink.classList.remove('show');
+  DropdownToggle.classList.remove('dropdownToggle');
+
+  // Toggle the selected dropdown
   if (action === 'open') {
-    var allElements = document.body.children;
-    // for (var i = 0; i < allElements.length; i++) {
-    //   allElements[i].classList.remove("blur");
-    // }
-
-    dropdown.classList.toggle('open');
-
-    // for (var i = 0; i < allElements.length; i++) {
-    //   if (allElements[i] !== dropdown) {
-    //     allElements[i].classList.add("blur");
-    //     header.classList.remove("blur");
-    //   }
-    // }
+    const isOpen = dropdown.classList.toggle('open');
+    if (isOpen) {
+      PricingLink.classList.add('show');
+      DropdownToggle.classList.add('dropdownToggle');
+    }
   }
 }
 
@@ -40,16 +55,53 @@ var searchInput = document.getElementById("searchInput");
 var searchSVG = document.getElementById("searchSVG");
 
 searchInput.addEventListener('click', function () {
-  searchSVG.style.display = "none";
-  searchInput.style.transition = "0.2s";
-  searchInput.style.paddingInline = "2%";
+  if (window.innerWidth >= 1200) {
+    searchSVG.style.display = "none";
+    searchInput.style.transition = "0.2s";
+    searchInput.style.paddingInline = "2%";
+  }
 })
 
 // Show SearchSVG onClick Document
 document.addEventListener('click', function (event) {
-  if (event.target !== searchInput && event.target !== searchSVG) {
-    searchSVG.style.display = "block";
-    searchInput.style.transition = "0.2s";
-    searchInput.style.paddingInline = "5%";
+  if(window.innerWidth >= 1200) {
+    if (event.target !== searchInput && event.target !== searchSVG) {
+      searchSVG.style.display = "block";
+      searchInput.style.transition = "0.2s";
+      searchInput.style.paddingInline = "5%";
+    }
   }
 });
+
+// Function to toggle the sidebar
+function toggleSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.classList.toggle('open');
+}
+
+document.querySelector('.close-btn').addEventListener('click', toggleSidebar);
+
+document.querySelector('.hamburger').addEventListener('click', toggleSidebar);
+
+// Function to open the sub-menu
+function openSubMenu(menuId, dropdown) {
+  const allSubMenus = document.querySelectorAll('.sub-menu');
+  allSubMenus.forEach(subMenu => {
+    if (subMenu.id !== menuId) {
+      subMenu.classList.remove('open');
+    }
+  });
+
+  const subMenu = document.getElementById(menuId);
+  const Dropdown = document.getElementById(dropdown);
+  subMenu.classList.add('open');
+  Dropdown.style.display = "block";
+}
+
+// Function to close the sub-menu
+function closeSubMenu() {
+  const allSubMenus = document.querySelectorAll('.sub-menu');
+  allSubMenus.forEach(subMenu => {
+    subMenu.classList.remove('open');
+  });
+}
