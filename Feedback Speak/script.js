af93287c872cd849816956171fc34e63865308ec
Shantanu <75296055+shantanusoni72@users.noreply.github.com>
2024-10-22 17:42:55 +0530

    Tab Navigation Section Upload
    Tab Navigation Section Upload Commit.

---
+++ Feedback Speak/script.js
@@ -0,0 +1,9 @@
+// Change Tabs
+function opentab(tabName) {
+    var i;
+    var x = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "flex";
+  }
